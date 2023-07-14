50177b49f352b10730d481d7938a07c9564e8726
kylemontgomery1 <kylemontgomery@wustl.edu>
2023-07-13 22:45:48 -0500

    copy together handler

--- custom_handler/text_generation_handler.py
+++ custom_handler/text_generation_handler.py
@@ -7,12 +7,15 @@ import numpy as np
 import random
 import timeit
 import zipfile
+import math
 
 import torch
 import transformers
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
+    StoppingCriteriaList,
+    StoppingCriteria
 )
 
 from ts.torch_handler.base_handler import BaseHandler
@@ -34,7 +37,7 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         """In this initialize function, the BERT model is loaded and
         the Layer Integrated Gradients Algorithm for Captum Explanations
         is initialized here.
-        Args:
+        requests:
             ctx (context): It is a JSON Object containing information
             pertaining to the model artefacts parameters.
         """
@@ -42,14 +45,15 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
         properties = ctx.system_properties
         model_dir = properties.get("model_dir")
         model_name = ctx.model_name
+        
+        self.max_batch_size = 1
+        self.deny_list = []
 
         self.device = torch.device(
             "cuda:" + str(properties.get("gpu_id"))
             if torch.cuda.is_available() and properties.get("gpu_id") is not None
             else "cpu"
         )
-        
-        logger.info("Device: %s", self.device)
         
         self.task_info = {
             "seed": 0,
@@ -70,128 +74,232 @@ class TransformersSeqClassifierHandler(BaseHandler, ABC):
             zip_ref.extractall(f"{model_dir}/{model_name}")
             
         logger.info("Extracting Tokenizer")
-        self.tokenizer = AutoTokenizer.from_pretrained(f"{model_dir}/{model_name}", use_fast=False)
+        self.tokenizer = AutoTokenizer.from_pretrained(f"{model_dir}/{model_name}")
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.tokenizer.padding_side = 'left'
+        self.tokenizer.truncation_side = 'left'
         
         logger.info("Extracting Model")
         self.model = AutoModelForCausalLM.from_pretrained(f"{model_dir}/{model_name}", torch_dtype=torch.float16)
-        
-        logger.info("Emptying Cache")
-        torch.cuda.empty_cache()
-        
-        logger.info("Moving model to device")
         self.model.to(self.device)
-        
-        logger.info("Setting model to evaluation mode")
-        self.model.eval()
+        torch.manual_seed(0)
         
         logger.info("Transformer model from path %s loaded successfully", model_dir)
         self.initialized = True
 
     def preprocess(self, requests):
         """Basic text preprocessing, based on the user's chocie of application mode.
-        Args:
+        requests:
             requests (str): The Input data in the form of text is passed on to the preprocess
             function.
         Returns:
             list : The preprocess function returns a list of Tensor for the size of the word tokens.
         """
         requests = requests[0]["body"]
-        logger.info(requests)
         requests = {k: v for k, v in requests.items() if v is not None}
         
         self.task_info["seed"] = get_int(requests.get("seed", 0), default=0)
-        self.task_info["prompt_seqs"] = [str(requests['prompt'])]
+        if isinstance(str(requests['prompt']), str):
+            self.task_info["prompt_seqs"] = [str(requests['prompt'])]
+        elif isinstance(str(requests['prompt']), list):
+            self.task_info["prompt_seqs"] = requests['prompt']
+        else:
+            logging.debug("wrong prompt format, it can only be str or list of str")
+            return
         self.task_info["output_len"] = get_int(requests.get("max_tokens", 16), default=16)
         self.task_info["beam_width"] = get_int(requests.get("beam_width", 1), default=1)
         self.task_info["top_k"] = get_int(requests.get("top_k", 50), default=50)
         self.task_info["top_p"] = get_float(requests.get("top_p", 0.0), default=0.0)
-        self.task_info["beam_search_diversity_rate"] = get_float(requests.get("beam_search_diversity_rate", 0.0),
-                                                                 default=0.0)
+        self.task_info["beam_search_diversity_rate"] = get_float(requests.get("beam_search_diversity_rate", 0.0), default=0.0)
         self.task_info["temperature"] = get_float(requests.get("temperature", 0.8), default=0.8)
         self.task_info["len_penalty"] = get_float(requests.get("len_penalty", 0.0), default=0.0)
         self.task_info["repetition_penalty"] = get_float(requests.get("repetition_penalty", 1.0), default=1.0)
         self.task_info["stop"] = requests.get("stop", [])
         self.task_info["logprobs"] = get_int(requests.get("logprobs", 0), default=0)
-        self.task_info["banned_tokens"] = requests.get("banned_tokens", [])
         
-        return self.tokenizer(self.task_info["prompt_seqs"], return_tensors="pt").to(self.device)
+        return None
+    
 
 
     def inference(self, inputs):
         """Predict the class (or classes) of the received text using the
         serialized transformers checkpoint.
-        Args:
+        requests:
             input_ids: Text Tensor from the pre-process function is passed here
         Returns:
             list : It returns the predicted value for the input text
         """
         
-        with torch.no_grad():
-            torch.manual_seed(self.task_info['seed'])
-            np.random.seed(self.task_info['seed'])
-            random.seed(self.task_info['seed'])
+        if len(self.task_info["prompt_seqs"][0]) == 0 or self.task_info["output_len"] == 0:
+            inference_result = []
+            item = {'choices': [], }
+            for beam_id in range(self.task_info["beam_width"]):
+                choice = {
+                    "text": '',
+                    "index": beam_id,
+                    "finish_reason": "length"
+                }
+                item['choices'].append(choice)
+            return item
             
-            input_length = inputs.input_ids.shape[1]
-            output_scores = self.task_info["logprobs"] > 0
-            
-            time = timeit.default_timer()
-            if self.task_info["temperature"] == 0:
-                outputs = self.model.generate(
-                    inputs["input_ids"], do_sample=True, top_p=self.task_info['top_p'],
-                    temperature=1.0, top_k=1,
-                    max_new_tokens=self.task_info["output_len"],
-                    return_dict_in_generate=True,
-                    output_scores=output_scores,  # return logit score
-                    output_hidden_states=False,  # return embeddings
-                    bad_words_ids = self.tokenizer(self.task_info["banned_tokens"], add_prefix_space=True, add_special_tokens=False).input_ids if self.task_info["banned_tokens"] else None,
-                )
-            else:
-                outputs = self.model.generate(
-                    inputs["input_ids"], do_sample=True, top_p=self.task_info['top_p'],
-                    temperature=self.task_info["temperature"],
-                    max_new_tokens=self.task_info["output_len"],
-                    return_dict_in_generate=True,
-                    output_scores=output_scores,  # return logit score
-                    output_hidden_states=False,  # return embeddings
-                    bad_words_ids = self.tokenizer(self.task_info["banned_tokens"], add_prefix_space=True, add_special_tokens=False).input_ids if self.task_info["banned_tokens"] else None,
-                )
-            if output_scores:
-                self.logprobs = convert_hf_score_to_logprobs(outputs.scores, self.task_info["logprobs"], self.tokenizer)
-            else:
-                self.logprobs = None
-
-            self.time_elapsed = timeit.default_timer() - time
-            
-        inference_result = []
-        item = {'choices': [], }
-        for beam_id in range(self.task_info["beam_width"]):
-            token = outputs.sequences[beam_id, input_length:]
-            output = self.tokenizer.decode(token)
-            choice = {
-                "text": post_processing_text(output, self.task_info["stop"]),
-                "index": beam_id,
-                "finish_reason": "length"
-            }
-            item['choices'].append(choice)
-            inference_result.append(item)
         
-        return inference_result
+        else:
+            complete_contexts = self.task_info["prompt_seqs"]
+            with torch.no_grad():
+                torch.manual_seed(self.task_info['seed'])
+                np.random.seed(self.task_info['seed'])
+                random.seed(self.task_info['seed'])
+                batch_size = min(len(complete_contexts), self.max_batch_size)
+                num_iter = math.ceil(len(complete_contexts) / batch_size)
+                output_buffer = []
+                logprobs_buffer = []
+                output_scores = self.task_info["logprobs"] > 0
+                if output_scores:
+                    logprobs_buffer = []
+                else:
+                    logprobs_buffer = None
+
+                time = timeit.default_timer()
+                for iter_i in range(num_iter):
+                    contexts = complete_contexts[iter_i * batch_size: (iter_i + 1) * batch_size]
+                    # Do translation
+                    contexts = [translate_chatml_to_openchat(context) for context in contexts]
+                    inputs = self.tokenizer(contexts, padding=True, truncation=True, return_tensors="pt").to(self.device)
+                    input_length = inputs.input_ids.shape[1]
+
+                    if self.task_info["temperature"] == 0:
+                        outputs = self.model.generate(
+                            **inputs, do_sample=False, 
+                            max_new_tokens=self.task_info["output_len"],
+                            return_dict_in_generate=True,
+                            output_scores=output_scores,  # return logit score
+                            output_hidden_states=True,  # return embeddings
+                            stream_tokens=self.task_info.get("stream_tokens"),
+                        )
+                    else:
+                        class StopWordsCriteria(StoppingCriteria):
+                            def __init__(self, stop_words, tokenizer):
+                                self.tokenizer = tokenizer
+                                self.stop_words = stop_words
+                                self._cache_str = ''
+
+                            def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+                                self._cache_str += self.tokenizer.decode(input_ids[0, -1])
+                                for stop_words in self.stop_words:
+                                    if stop_words in self._cache_str:
+                                        return True
+                                return False
+    
+                        outputs = self.model.generate(
+                            **inputs, 
+                            do_sample=True, 
+                            top_p=self.task_info['top_p'],
+                            top_k=self.task_info['top_k'],
+                            repetition_penalty=self.task_info['repetition_penalty'],
+                            temperature=self.task_info["temperature"],
+                            max_new_tokens=self.task_info["output_len"],
+                            return_dict_in_generate=True,
+                            output_scores=output_scores,  # return logit score
+                            output_hidden_states=True,  # return embeddings
+                            stream_tokens=self.task_info.get("stream_tokens"),
+                            stopping_criteria=StoppingCriteriaList([StopWordsCriteria(self.task_info["stop"], self.tokenizer)]) if self.task_info.get("stop") else None,
+                        )
+                    if output_scores:
+                        ### hard code, assume bsz==1
+                        n_logprobs = self.task_info["logprobs"]
+        
+                        # sampled tokens
+                        token_ids = outputs.sequences[0, inputs['input_ids'].size(1):].tolist()
+                        tokens = self.tokenizer.convert_ids_to_tokens(token_ids)
+
+                        logprobs_dict = {
+                            'tokens': tokens,
+                            'token_logprobs': [],
+                            'top_logprobs': [],
+                        }
+
+                        # last layer hidden states
+                        hids = [outputs.hidden_states[0][-1][:, -1:]]
+                        hids += [hid[-1] for hid in outputs.hidden_states[1:]]
+                        hids = torch.cat(hids, dim=1)
+                        # origianl logits
+                        logits = self.model.get_output_embeddings()(hids)
+                        logprobs = logits.log_softmax(-1)
+                        values, indices = logprobs.topk(n_logprobs, dim=-1)
+
+                        for i in range(indices.size(1)):
+                            selected_token_id = token_ids[i]
+                            # topk tokens
+                            tokens = self.tokenizer.convert_ids_to_tokens(indices[0, i])
+                            # topk scores
+                            scores = values[0, i].tolist()
+
+                            logprobs_dict['token_logprobs'].append(logprobs[0, i, selected_token_id].item())
+                            logprobs_dict['top_logprobs'].append({
+                                t: s for t,s in zip(tokens, scores)
+                            })
+                            
+                        logprobs_buffer.append(logprobs_dict)
+                        
+                    output_buffer.append(outputs)
+                time_elapsed = timeit.default_timer() - time
+                
+            logging.debug(f"Inference time costs: {time_elapsed} ms.")
+
+            if len(complete_contexts) == 1:
+                item = {'choices': [], }
+                for beam_id in range(self.task_info["beam_width"]):
+
+                    token = outputs.sequences[beam_id, input_length:]  # exclude context input from the output
+                    logging.debug(f"[INFO] raw token: {token}")
+                    output = self.tokenizer.decode(token)
+                    logging.debug(f"[INFO] beam {beam_id}: \n[Context]\n{contexts}\n\n[Output]\n{output}\n")
+                    choice = {
+                    "text": post_processing_text(output, self.task_info["stop"], self.deny_list),
+                    "index": beam_id,
+                    "finish_reason": "length"
+                }
+                if output_scores:
+                    choice['logprobs'] = logprobs_buffer[0]
+                item['choices'].append(choice)
+                self.time_elapsed = time_elapsed
+            else:
+                item = {'choices': [], }
+                for i_output, outputs in enumerate(output_buffer):
+                    beam_width = self.task_info["beam_width"]
+                    current_batch_size = outputs.sequences.shape[0] // beam_width
+                    for sample_id in range(current_batch_size):
+
+                        for beam_id in range(beam_width):
+                            token = outputs.sequences[sample_id * beam_width + beam_id, input_length:]
+                            logging.debug(f"[INFO] raw token: {token}")
+                            output = self.tokenizer.decode(token)
+                            logging.debug(f"[INFO] beam {beam_id}: \n[Context]\n{contexts}\n\n[Output]\n{output}\n")
+                            choice = {
+                                "text": post_processing_text(output, self.task_info["stop"], self.deny_list),
+                                "index": beam_id,
+                                "finish_reason": "length"+str(sample_id)
+                            }
+                            if output_scores:
+                                choice['logprobs'] = logprobs_buffer[i_output]
+                            item['choices'].append(choice)
+                self.time_elapsed = time_elapsed
+                
+        return item
+                        
 
     def postprocess(self, inference_results):
         """Post Process Function converts the predicted response into Torchserve readable format.
-        Args:
+        requests:
             inference_results (list): It contains the predicted response of the input text.
         Returns:
             (list): Returns a list of the Predictions and Explanations.
         """
-        
         result = {
-            "choices": inference_results[0]['choices'],
+            "choices": inference_results['choices'],
             "raw_compute_time": self.time_elapsed,
         }
-        
-        if self.task_info["logprobs"] > 0:
-            result['logprobs'] = self.logprobs
         
         return [result]
     
@@ -258,3 +366,14 @@ def convert_hf_score_to_logprobs(scores, k, tokenizer):
         results.append(logprobs)
     return results
 
+def translate_chatml_to_openchat(prompt):
+    prompt = prompt.replace('<|im_start|>system\n', '<human>: ')
+    prompt = prompt.replace('<|im_start|>user\n', '<human>: ')
+    prompt = prompt.replace('<|im_start|>assistant\n', '<bot>: ')
+    prompt = prompt.replace('<|im_start|>user', '<human>:')
+    prompt = prompt.replace('<|im_start|>assistant', '<bot>:')
+    prompt = prompt.replace('\n<|im_end|>', '')
+    prompt = prompt.replace('<|im_end|>', '')
+    prompt = prompt.rstrip()
+    # print(prompt)
+    return prompt
