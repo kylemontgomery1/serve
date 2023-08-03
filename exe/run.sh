212d2a437551cf2c63644617cb9a9d135cf1ef13
Kyle Montgomery <kylemontgomery@wustl.edu>
2023-08-03 12:56:52 -0500

    adding model_store to git track

--- exe/run.sh
+++ exe/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bsub -J "Inference API" -m chenguang02 -gpu "num=4:gmodel=NVIDIARTXA6000:mode=exclusive_process" /bin/bash /scratch/serve/exe/launch_docker.sh
+bsub -J "Inference API" -m chenguang02 -gpu "num=8:gmodel=NVIDIARTXA6000:gmem=48" /bin/bash /scratch/serve/exe/launch_docker.sh
